a007fa851a5a7c073cfa6a6cbdcdc80efa2b1a20
SnailRun <3074994545@qq.com>
2024-11-25 22:03:20 +0800

    feat: 移出不需要的内容

---
+++ src/components/Count.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>
+    {{ counterStore.count }}
+  </h2>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+const counterStore = useCounterStore()
+</script>
